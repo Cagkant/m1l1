8f56bcf902db8b22499147afbb5afd969a00a941
Cagkant <cagkan.tilkan14@gmail.com>
2025-10-24 22:25:46 +0300

    Temizlik botu
    poşeti topla

---
+++ bot.py
@@ -0,0 +1,106 @@
+# This example requires the 'members' and 'message_content' privileged intents to function.
+
+import requests
+import os
+import discord
+from discord.ext import commands
+import random 
+
+
+
+description = """An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here."""
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    # Tell the type checker that User is filled up at this point
+    assert bot.user is not None
+
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    # Joined at can be None in very bizarre cases so just handle that as well
+    if member.joined_at is None:
+        await ctx.send(f'{member} has no join date.')
+    else:
+        await ctx.send(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')
+
+
+easy_tasks = ["Başkalarını bilinçlendirmek", "Evimizdeki basit atıkları geri dönüşüme atmak"]
+medium_tasks = ["Çöpe gidecek eşyaları yeniden kullanmak", "Eve bir geri dönüşüm kutusuna at", "Çöp kutusunun yakınındaki çöpleri çöp kutusua at."]
+hard_tasks = ["Çöp toplama işine girişmek", "alıştıma yapmak için her gün 1 poşet çöp topla,", "mutfaktaki atıkların hepsini dışarı çıkart"]
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+@bot.command()
+async def kolay_seviye(ctx):
+    await ctx.send(random.choice(easy_tasks))
+@bot.command()
+async def orta_seviye(ctx):
+    await ctx.send(random.choice(medium_tasks))
+@bot.command()
+async def zor_seviye(ctx):
+    await ctx.send(random.choice(hard_tasks))
+
+bot.run("token")
